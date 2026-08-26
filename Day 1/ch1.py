d820bddb64f61ce4b03337dc9500bcd850d01bb0
GuruB-Codes <gurulingappaofficial@gmail.com>
2026-08-26 21:23:21 +0530

    ch1

---
+++ Day 1/ch1.py
@@ -0,0 +1,12 @@
+#Create variables for: Your name Your ageYour college Your branch Then print all four.
+
+name="Guru"
+age=21
+college="VIT"
+branch="Computer Science"
+
+
+print("Name:", name)
+print("Age:", age)
+print("College:", college)
+print("Branch:", branch)
