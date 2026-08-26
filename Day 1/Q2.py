5b708d385dbf8f1102810d0a7fb094198f1e5325
GuruB-Codes <gurulingappaofficial@gmail.com>
2026-08-26 21:19:01 +0530

    Q2

---
+++ Day 1/Q2.py
@@ -0,0 +1,13 @@
+a=10
+b=20
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+mod=a%b
+
+print("Addition:", add)
+print("Subtraction:", sub)
+print("Multiplication:", mul)
+print("Division:", div)
+print("Modulus:", mod)
