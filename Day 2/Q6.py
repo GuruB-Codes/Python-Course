9450fb84aa54ff055645696e6dc279bb5ff5ff85
GuruB-Codes <gurulingappaofficial@gmail.com>
2026-08-27 10:31:52 +0530

    Operators

---
+++ Day 2/Q6.py
@@ -0,0 +1,40 @@
+#Arithmetic Operators
+a=10    
+b=3
+print(a+b)
+print(a-b)
+print(a*b)  
+print(a/b)
+print(a//b)  # floor division
+print(a%b)
+print(a**b)  
+
+#Assignment Operators
+c=5  
+print(c)
+c+=3
+print(c)
+c-=2
+print(c)
+c*=4
+print(c)
+c/=2
+print(c)
+
+#Comparison Operators
+x=10
+y=5
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
+print(x==y)
+print(x!=y)
+
+#Logical Operators
+p=True
+q=False
+print(p and q)
+print(p or q)
+print(not p)
+
