5085a973e1f52bee79ef37282906df55fd95be62
GuruB-Codes <gurulingappaofficial@gmail.com>
2026-08-27 10:24:27 +0530

    Q5

---
+++ Day 2/Q5.py
@@ -0,0 +1,10 @@
+#name=input("Enter your name: ")
+#print (name)
+
+#age=int(input("Enter your age: ")) # input function always returns string, so we need to convert it into int using int() function
+#print (age)
+#print (type(age))
+
+marks=float(input("Enter your marks: "))  # input function always returns string, so we need to convert it into float using float() function
+print (marks)
+print (type(marks))
